ac6cc202ebd185bb7cbc3da3fb98d25426233ef0
cyoung <cy880708@163.com>
2019-06-27 14:38:33 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spring Boot Frank #
+
+## 前言 ##
+对Spring Boot可以做的事情做一些汇总。
+
+|项目|简介|介绍|
+|:--|:--|:--|
+|spring-boot-log|日志处理|了解spring boot如何记录日志。|
---
+++ spring-boot-log/src/main/java/com/frank/sb/log/LogApplication.java
@@ -0,0 +1,21 @@
+package com.frank.sb.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @description：
+ * @author: Yang.Chang
+ * @project: spring-boot-frank
+ * @package: com.frank.sb.log、
+ * @email: cy880708@163.com
+ * @date: 2019/6/27 下午2:35
+ * @mofified By:
+ */
+@SpringBootApplication
+public class LogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class, args);
+    }
+}
