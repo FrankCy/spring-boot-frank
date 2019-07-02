1f7fe4d6a01e463f1e9322e4c8d6b0e50aa5e052
cyoung <cy880708@163.com>
2019-07-02 17:37:45 +0800

    提交自定义端点

--- spring-boot-actuator/src/main/java/com/frank/sb/actuator/ActuatorApplication.java
+++ spring-boot-actuator/src/main/java/com/frank/sb/actuator/ActuatorApplication.java
@@ -1,21 +1,33 @@
 package com.frank.sb.actuator;
 
+import com.frank.sb.actuator.config.MyEndPoint;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
- * @version 1.0
- * @description：
- * @author: Yang.Chang
- * @project: spring-boot-frank
- * @package: com.frank.sb.actuator、
- * @email: cy880708@163.com
- * @date: 2019/6/28 上午10:21
- * @mofified By:
+ * 启动类
  */
 @SpringBootApplication
 public class ActuatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
     }
+
+    /**
+     * 启动类中，添加下面函数，将自定义端点交由spring进行管理，令其生效
+     */
+    @Configuration
+    static class MyEndpointConfiguration {
+        @Bean
+        @ConditionalOnMissingBean
+        @ConditionalOnEnabledEndpoint
+        public MyEndPoint myEndPoint() {
+            return new MyEndPoint();
+        }
+    }
+
 }
